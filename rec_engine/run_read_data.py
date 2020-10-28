440665860ad272698defff2b8e1d50f39e4fcd26
fei Wang <wafei@wafei-00.local>
2020-10-27 21:46:19 -0400

    read_data.py

--- rec_engine/run_read_data.py
+++ rec_engine/run_read_data.py
@@ -7,11 +7,11 @@ from pathlib import Path
 import pandas as pd
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
-pd.set_option('display.max_colwidth', 500)
+pd.set_option('display.max_colwidth', None)
 PARENT_DIR = Path(__file__).parents[0]
 
 
-def run(infile: str):
+def run(infile, field_name, literal_eval=True):
     rec = RecEngine()
     row = []
     file_name = PARENT_DIR / 'data' / 'google_local_review' / infile
@@ -19,18 +19,48 @@ def run(infile: str):
         lines = f.readlines()
         i = 1
         for line in lines:
-            y = json.loads(json.dumps(ast.literal_eval(line)))
+            try:
+                if literal_eval:
+                    y = json.loads(json.dumps(ast.literal_eval(line)))
+                else:
+                    y = json.loads(line)
+
+                for field in list(y.keys()):
+                    if field not in field_name:
+                        del y[field]
+                    elif isinstance(y[field], list) and isinstance(y[field][0], str):
+                        try:
+                            # y[field] = ','.join(y[field])
+                            y[field] = y[field][len(y[field]) - 1]
+                        except:
+                            y[field] = y[field][0]
+                    elif isinstance(y[field], list) and isinstance(y[field][0], list):
+                        y[field] = y[field][0][1]
+
+            except ValueError:
+                continue
             row.append(y)
-            i = i + 1
-            if i % 500 == 0:
+            if i % 10000 == 0:
                 logger.info(f"i={i}")
+            i = i + 1
 
     res = pd.DataFrame(row)
     print(res[:10])
     return res
 
+def row_transform():
+
+    pass
 
 if __name__ == '__main__':
-#    places = run('first.1000.places.clean.json')
-#    reviews = run('first.1000.reviews.clean.json')
-    users = run('first.1000.users.clean.json')
+    places = run('places.clean.json', ['gPlusPlaceId', 'gps', 'price', 'closed'], True)
+    reviews = run('reviews.clean.json', ['gPlusPlaceId', 'reviewTime', 'rating', 'categories', 'gPlusUserId'], True)
+    reviews.reviewTime = pd.to_datetime(reviews.reviewTime, format='%b %d, %Y')
+    reviews = reviews.sort_values(['reviewTime'], ascending=[True])
+
+    users = run('users.clean.json', ['gPlusUserId', 'jobs', 'currentPlace', 'previousPlace'], False)
+
+
+    dat = reviews.merge(places, on='gPlusPlaceId').merge(users, on='gPlusUserId')
+    dat.to_csv('google_local_review.csv')
+
